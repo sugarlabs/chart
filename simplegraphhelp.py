04fc06660c04a5eed83419e037d69a33176261ce
Gonzalo Odiard <godiard@gmail.com>
2012-03-07 16:00:09 -0300

    Correct gramatical errors in the help text
    Signed-off-by: Gonzalo Odiard <gonzalo@laptop.org>

--- simplegraphhelp.py
+++ simplegraphhelp.py
@@ -10,14 +10,14 @@ def create_help(toolbar):
     toolbar.insert(helpitem, -1)
     helpitem.show()
     helpitem.add_section(_('Basic usage'))
-    helpitem.add_paragraph(_('First you need add data to create the graphic'))
+    helpitem.add_paragraph(_('First you need add data to create the graph'))
     helpitem.add_paragraph(_('You can add data with this button'),
             'row-insert')
     helpitem.add_paragraph(_('...or remove data with this button'),
             'row-remove')
-    helpitem.add_paragraph(_('To change the graphic title, just change the activity title'))
+    helpitem.add_paragraph(_('To change the graph title, just change the activity title'))
 
-    helpitem.add_paragraph(_('Next you can change the type of graphic'))
+    helpitem.add_paragraph(_('Next you can change the type of graph'))
     helpitem.add_paragraph(_('Vertical bars'), 'vbar')
     helpitem.add_paragraph(_('Horizontal bars'), 'hbar')
     helpitem.add_paragraph(_('Lines'), 'line')
@@ -27,8 +27,8 @@ def create_help(toolbar):
     helpitem.add_paragraph(_('You can change the colors or the horizontal and vertical labels in the configs toolbar'),
             'preferences-system')
 
-    helpitem.add_section(_('Saving as a image'))
-    helpitem.add_paragraph(_('In the activity toolbar you have button to save the graphic as a image'),
+    helpitem.add_section(_('Saving as an image'))
+    helpitem.add_paragraph(_('In the activity toolbar you have button to save the graph as an image'),
             'save-as-image')
 
     helpitem.add_section(_('Reading data from other activities'))
